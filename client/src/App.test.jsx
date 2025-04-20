0d388e32d73db987e8421c7673715d277f5009ce
Prathvihan1008 <prathvihan1008@gmail.com>
2025-04-20 17:54:11 +0530

    github actions check

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -12,21 +12,3 @@ test("renders Task Tracker heading", () => {
 	);
 	expect(screen.getByText(/task tracker/i)).toBeInTheDocument();
 });
-
-// test("creates and deletes a task", async () => {
-// 	render(<App />);
-
-// 	const input = screen.getByPlaceholderText(/enter a task/i);
-// 	fireEvent.change(input, { target: { value: "Buy milk" } });
-// 	fireEvent.click(screen.getByText(/add task/i));
-
-// 	const taskItem = await screen.findByText("Buy milk");
-// 	expect(taskItem).toBeInTheDocument();
-
-// 	const deleteButton = screen.getByText("Delete");
-// 	fireEvent.click(deleteButton);
-
-// 	await waitFor(() => {
-// 		expect(screen.queryByText("Buy milk")).not.toBeInTheDocument();
-// 	});
-// });
